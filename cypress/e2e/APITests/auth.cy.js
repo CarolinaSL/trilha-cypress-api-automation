cd78c99c45bce139c1a8b7350ca63fb10c8363b9
Venilton FalvoJr <falvojr@gmail.com>
2023-10-26 17:35:09 -0300

    Try to fix auth.cy.js
    Try to fix test "3 - POST credentials to auth endpoint with invalid credentials".

--- cypress/e2e/APITests/auth.cy.js
+++ cypress/e2e/APITests/auth.cy.js
@@ -60,9 +60,8 @@ describe('auth api testing', () => {
         };
 
         cy.postRequest("/auth", {"Content-type": "application/json"},  body ).then((response) => {
-            expect(response.status).to.eq(200);
-            expect(response.body).to.have.property('token').and.to.be.a('string');
-            expect(response.body).not.to.be.empty;
+            expect(response.status).to.eq(401);
+            expect(response.body).to.have.property('reason', 'Bad credentials');
         });
     });
 });
